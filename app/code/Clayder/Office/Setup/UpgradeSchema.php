41863ae0f6638823fef6eabdeaa5fee46a8ade50
Peter Clayder <peterclayder@gmail.com>
2019-05-02 23:03:52 -0300

    [module/office][added] UpgradeSchema

--- app/code/Clayder/Office/Setup/UpgradeSchema.php
+++ app/code/Clayder/Office/Setup/UpgradeSchema.php
@@ -19,6 +19,22 @@ class UpgradeSchema implements UpgradeSchemaInterface
      */
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-
+        $setup->startSetup();
+        $employeeEntityTable = \Clayder\Office\Model\Employee::ENTITY.'_entity';
+        $departmentEntityTable = "clayder_office_department";
+        $setup->getConnection()
+            ->addForeignKey(
+                $setup->getFkName(
+                    $employeeEntityTable,
+                    'department_id',
+                    $departmentEntityTable,
+                    'entity_id'
+                ),
+                'department_id',
+                $setup->getTable($departmentEntityTable),
+                'entity_id',
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+            );
+        $setup->endSetup();
     }
 }
